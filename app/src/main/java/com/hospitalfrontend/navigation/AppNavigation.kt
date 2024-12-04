1ff5e0f8d962b81fdbbef6168a4dfdd709f9a0b0
Jordi Vidal <163132865+reivaX1755@users.noreply.github.com>
2024-12-04 16:07:01 +0100

    #9 Structure ViewModel created NOT IMPLEMENTED style to login screen, register screen created & style set

--- app/src/main/java/com/hospitalfrontend/navigation/AppNavigation.kt
+++ app/src/main/java/com/hospitalfrontend/navigation/AppNavigation.kt
@@ -6,18 +6,20 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.hospitalfrontend.ui.home.HomeScreen
-import com.hospitalfrontend.ui.nurse.all.AllNursesScreen
-import com.hospitalfrontend.ui.nurse.byname.FindNurseScreen
-import com.hospitalfrontend.ui.nurse.login.NurseLoginScreen
+import com.hospitalfrontend.ui.nurseinfo.all.AllNursesScreen
+import com.hospitalfrontend.ui.nurseinfo.byname.FindNurseScreen
+import com.hospitalfrontend.ui.authentication.NurseLoginScreen
+import com.hospitalfrontend.ui.authentication.NurseRegisterScreen
 
 @Composable
 fun AppNavigation() {
     val navController: NavHostController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "home") {
+    NavHost(navController = navController, startDestination = "login_nurse") {
         composable("home") { HomeScreen(navController) }
         composable("all_nurses") { AllNursesScreen(navController) }
         composable("find_nurse") { FindNurseScreen(navController) }
         composable("login_nurse") { NurseLoginScreen(navController) }
+        composable("register_nurse") { NurseRegisterScreen(navController) }
     }
 }
---
+++ app/src/main/java/com/hospitalfrontend/ui/authentication/NurseRegisterScreen.kt
@@ -0,0 +1,193 @@
+package com.hospitalfrontend.ui.authentication
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.hospitalfrontend.R
+
+
+@Composable
+fun NurseRegisterScreen(navController: NavController) {
+    var name by remember { mutableStateOf(TextFieldValue("")) }
+    var username by remember { mutableStateOf(TextFieldValue("")) }
+    var password by remember { mutableStateOf(TextFieldValue("")) }
+    var isError by remember { mutableStateOf(false) }
+    var isLoginSuccess by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .background(Color(0xFFE73843)),
+                contentAlignment = Alignment.TopCenter
+            ) {
+                Box(
+                    modifier = Modifier
+                        .height(200.dp)
+                        .width(100.dp)
+                        .padding(top = 100.dp)
+                        .background(color = Color.White, shape = MaterialTheme.shapes.medium)
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.cruz_home),
+                    contentDescription = "Cruz Home",
+                    modifier = Modifier
+                        .size(200.dp)
+                        .padding(top = 100.dp)
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .background(Color.Black)
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .background(
+                        color = Color.DarkGray,
+                        shape = MaterialTheme.shapes.medium
+                    )
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Crear Cuenta",
+                    style = MaterialTheme.typography.headlineLarge,
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp)
+                )
+
+                TextField(
+                    value = name,
+                    onValueChange = {
+                        name = it
+                        isError = false
+                    },
+                    label = { Text("Nombre") },
+                    modifier = Modifier.fillMaxWidth(),
+                    singleLine = true,
+                    isError = isError,
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                TextField(
+                    value = username,
+                    onValueChange = {
+                        username = it
+                        isError = false
+                    },
+                    label = { Text("Usuario") },
+                    modifier = Modifier.fillMaxWidth(),
+                    singleLine = true,
+                    isError = isError,
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                TextField(
+                    value = password,
+                    onValueChange = {
+                        password = it
+                        isError = false
+                    },
+                    label = { Text("Contraseña") },
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    modifier = Modifier.fillMaxWidth(),
+                    singleLine = true,
+                    isError = isError,
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = {
+                        if (validateCredentials(username.text, password.text)) {
+                            isLoginSuccess = true
+                            isError = false
+                        } else {
+                            isError = true
+                            isLoginSuccess = false
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE73843)),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Crear Cuenta", color = Color.White)
+                }
+
+                Box(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .clickable {
+                            navController.navigate("login_nurse")
+                        }
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = 10.dp)
+                ) {
+                    Text(
+                        text = "¿Ya tienes cuenta? Iniciar sesión",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = Color.White,
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+                if (isError) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "Usuario o contraseña incorrectos. Intenta nuevamente.",
+                        color = Color.Red,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+                if (isLoginSuccess) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "Login correcto",
+                        color = Color.Green,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    }
+}
