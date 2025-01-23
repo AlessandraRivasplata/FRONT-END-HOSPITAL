8db23e2ac824f1d4890f7f781ea39736b31945df
Getsu05 <scortacansmartin@gmial.com>
2025-01-23 16:14:18 +0100

    Conect register screen with backend endpoint createNurse #32

--- app/src/main/java/com/hospitalfrontend/retrofitconfig/RemoteMessageInterface.kt
+++ app/src/main/java/com/hospitalfrontend/retrofitconfig/RemoteMessageInterface.kt
@@ -3,8 +3,14 @@ package com.hospitalfrontend.retrofitconfig
 import com.hospitalfrontend.model.Nurse
 import com.hospitalfrontend.model.NurseResponse
 import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
 
 interface RemoteMessageInterface {
     @GET("/nurse/allnurses")
     suspend fun getAllNurses(): NurseResponse
+
+    @Headers("Accept: application/json","Content-Type: application/json")
+    @POST("nurse")
+    suspend fun createNurse(nurse: Nurse): NurseResponse
 }
