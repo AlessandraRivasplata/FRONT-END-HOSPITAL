a0ecafb010be363b31bf474891cbf83798c4406c
Jordi Vidal <163132865+reivaX1755@users.noreply.github.com>
2025-02-04 16:45:36 +0100

    new splash screen

--- app/src/main/java/com/hospitalfrontend/ui/home/HomeScreen.kt
+++ app/src/main/java/com/hospitalfrontend/ui/home/HomeScreen.kt
@@ -2,30 +2,22 @@ package com.hospitalfrontend.ui.home
 
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.hospitalfrontend.R
 import com.bumptech.glide.integration.compose.GlideImage
-import com.hospitalfrontend.ui.authentication.NurseAuthViewModel
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
